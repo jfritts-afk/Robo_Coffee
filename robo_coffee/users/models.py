4e05db17e9616c9d3a726b2f26bffa0f3c8cced7
Joshua <143011032+jfritts-afk@users.noreply.github.com>
2024-08-08 15:29:26 -0400

    updated readme

--- robo_coffee/users/models.py
+++ robo_coffee/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    google_calendar_token = models.TextField(blank=True, null=True)
+    weather_api_key = models.CharField(max_length=255, blank=True, null=True)
+    sms_api_key = models.CharField(max_length=255, blank=True, null=True)
+    preferences = models.JSONField(default=dict, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
